1eabf02ecdd7d442ef476e85de25cc4e37397008
chatton <cianhatton@gmail.com>
2017-10-04 21:32:36 +0100

    added direction object that contains name of direction and angle, flipped pacman on going left

--- js/script.js
+++ js/script.js
@@ -12,6 +12,10 @@ function Pacman(x, y, radius, speed){
     // mout animation object in charge of providing the "angle"
     // variable to add/remove to the ctx.arc of drawing pacman
     // to make the mouth open and close.
+    this.direction = {
+        name : "RIGHT",
+        angle : 0
+    };
     this.mouthAnimation = {
         speed : 0.05,
         gap : 0.3,
@@ -38,13 +42,19 @@ function Pacman(x, y, radius, speed){
     this.x = x;
     this.y = y;
     this.radius = radius;
-    angle = 0;
     this.draw = function(){
         ctx.save();
         ctx.translate(this.x, this.y); // translates the entire co-ordinate system, x, y are our new 0,0
-        ctx.rotate(angle); // rotate the entire canvas
+        // want to flip only if going left, otherwise, the eye is on the bottom left of pacman.
+        // We want it on the top left. So a mirror image of facing right is perfect for this.
+        if(this.direction.name == "LEFT"){  
+            ctx.scale(1,-1);
+        }
+        ctx.rotate(this.direction.angle); // rotate the entire canvas
         ctx.translate(-this.x, -this.y); // offset based on the initial translation            
         
+ 
+
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, Math.PI / 4 - this.mouthAnimation.angle, Math.PI * 1.75 + this.mouthAnimation.angle);
         ctx.lineTo(this.x, this.y);
@@ -91,6 +101,7 @@ function Pacman(x, y, radius, speed){
 
     },
     this.lockToScreen = function(){
+        // if pacman every goes offscreen, he'll be locked back into the canvas.
         if(this.x + this.radius >= canvas.width){
             this.x = canvas.width - this.radius;
         }
@@ -110,22 +121,34 @@ function Pacman(x, y, radius, speed){
     this.move = function(keyCode){
         switch(keyCode){
             case 38: // UP
+                this.direction = {
+                    name : "UP",
+                    angle : Math.PI * (3/2)
+                };
                 this.speed.dy = -1;
                 this.speed.dx = 0;
-                angle = Math.PI * (3/2);
                 break;
             case 37: // LEFT
-                angle = Math.PI;
+                this.direction = {
+                    name : "LEFT",
+                    angle : Math.PI
+                };
                 this.speed.dx = -1;
                 this.speed.dy = 0;
                 break;
             case 40: // DOWN
-                angle = Math.PI / 2;
+                this.direction = {
+                    name: "DOWN",
+                    angle : Math.PI / 2
+                };
                 this.speed.dx = 0;
                 this.speed.dy = 1;
                 break;
             case 39: // RIGHT
-                angle = 0;
+                this.direction = {
+                    name : "RIGHT",
+                    angle : 0
+                };
                 this.speed.dx = 1;
                 this.speed.dy = 0;
         }
@@ -135,7 +158,6 @@ function Pacman(x, y, radius, speed){
 var pacman = new Pacman(100, 100, 25);
 
 function start(){
-
     ctx.clearRect(0,0,canvas.height, canvas.width);
     pacman.draw();
     pacman.update();
